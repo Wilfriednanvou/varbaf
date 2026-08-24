cff60b6f2f5e8480d9cc657f3eac558627a4ca8c
Wilfriednanvou <wilfriednanvou@gmail.com>
2026-08-24 04:02:48 +0100

    Contourne le bug de symfony/html-sanitizer sous PHP 8.3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Contournement d'un bug de symfony/html-sanitizer (utilisé par Filament)
+        // sur PHP 8.3 qui tente d'appeler Dom\HTMLDocument (introduit en PHP 8.4).
+        Str::macro('sanitizeHtml', function (string $html): string {
+            return strip_tags($html, ['b', 'i', 'u', 's', 'strong', 'em', 'a', 'br', 'ul', 'ol', 'li', 'p', 'span']);
+        });
     }
 }
